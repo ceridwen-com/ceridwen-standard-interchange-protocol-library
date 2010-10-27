9ecaf428b7f68b0c3eb36ec7c80fba99b052576a
Matthew J. Dovey <m.dovey@ceridwen.com>
2010-10-27 10:55:37 +0000

    Updated copyright notices to GPL3 Added SecurityDevice.num to build scripts Changed failed SecurityDevice to warning and explicit mention defaulting to null device Changed config to load null Security Device

--- src/com/ceridwen/circulation/SIP/exceptions/MandatoryFieldOmitted.java
+++ src/com/ceridwen/circulation/SIP/exceptions/MandatoryFieldOmitted.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Matthew J. Dovey.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Matthew J. Dovey - initial API and implementation
+ ******************************************************************************/
 /**
  * <p>Title: Self Issue</p>
  * <p>Description: Self Issue Client</p>
