01d30481a4923c35e0c172d44c3d59850cd23ddc
Matthew J. Dovey <m.dovey@ceridwen.com>
2017-06-16 15:45:45 +0100

    add message validation utilities

---
+++ src/main/java/com/ceridwen/circulation/SIP/util/MessageValidator.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ceridwen.circulation.SIP.util;
+
+import com.ceridwen.circulation.SIP.exceptions.InvalidFieldLength;
+import com.ceridwen.circulation.SIP.exceptions.MandatoryFieldOmitted;
+import com.ceridwen.circulation.SIP.exceptions.MessageNotUnderstood;
+import com.ceridwen.circulation.SIP.messages.ACSStatus;
+import com.ceridwen.circulation.SIP.messages.CheckIn;
+import com.ceridwen.circulation.SIP.messages.CheckOut;
+import com.ceridwen.circulation.SIP.messages.Message;
+
+/**
+ *
+ * @author Matthew.Dovey
+ */
+public class MessageValidator {
+  public enum condition {
+    MESSAGE_VALIDATED,
+    MESSAGE_NOT_SUPPORTED,
+    CHECKOUT_DISABLED,
+    CHECKIN_DISABLED,
+    OFFLINE_NOT_SUPPORTED,
+    PATRON_STATUS_UPDATE_NOT_SUPPORTED,
+    RENEWALS_NOT_SUPPORTED,
+    MISSING_MANDATORY_FIELDS, 
+    INVALID_FIELD_LENGTH,
+    UNKNOWN_MESSAGE 
+  }
+  
+  
+  private ACSStatus status;
+  
+  public MessageValidator(ACSStatus status) {
+    this.status = status;
+  }
+  
+  public condition isMessageValid(Message message) {
+    condition cond = isMessageSupported(message.getClass());
+    
+    if (cond == condition.MESSAGE_VALIDATED) {
+      try {
+        String test = message.encode();
+      } catch (MandatoryFieldOmitted ex) {
+        return condition.MISSING_MANDATORY_FIELDS;
+      } catch (InvalidFieldLength ex) {
+        return condition.INVALID_FIELD_LENGTH;
+      } catch (MessageNotUnderstood ex) {
+        return condition.UNKNOWN_MESSAGE;
+      }
+
+      if (!status.isOfflineOk()) {
+        if (message instanceof CheckIn) {
+          if (((CheckIn)message).isNoBlock()) {
+            return condition.OFFLINE_NOT_SUPPORTED;
+          }
+        }
+        if (message instanceof CheckOut) {
+          if (((CheckOut)message).isNoBlock()) {
+            return condition.OFFLINE_NOT_SUPPORTED;
+          }
+        }
+      }
+      return cond;
+    } else {
+      return cond;
+    }
+  }
+  
+  public condition isMessageSupported(Class<? extends Message> clazz) {    
+    switch (clazz.getName()) {
+      case "ACSResend":
+      case "SCResend":
+        return status.getSupportedMessages().isRequestScAcsResend()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "ACSStatus":
+      case "SCStatus":
+        return status.getSupportedMessages().isScAcsStatus()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "BlockPatron":
+        return status.getSupportedMessages().isBlockPatron()?status.isStatusUpdateOk()?condition.MESSAGE_VALIDATED:condition.PATRON_STATUS_UPDATE_NOT_SUPPORTED:condition.MESSAGE_NOT_SUPPORTED;
+      case "CheckIn":
+      case "CheckInResponse":
+        return status.getSupportedMessages().isCheckIn()?status.isCheckInOk()?condition.MESSAGE_VALIDATED:condition.CHECKIN_DISABLED:condition.MESSAGE_NOT_SUPPORTED;
+      case "CheckOut":
+      case "CheckOutResponse":
+        return status.getSupportedMessages().isCheckOut()?status.isCheckOutOk()?condition.MESSAGE_VALIDATED:condition.CHECKOUT_DISABLED:condition.MESSAGE_VALIDATED;
+      case "EndPatronSession":
+      case "EndSessionResponse":
+        return status.getSupportedMessages().isEndPatronSession()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "FeePaid":
+      case "FeePaidResponse":
+        return status.getSupportedMessages().isFeePaid()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "Hold":
+      case "HoldResponse":
+        return status.getSupportedMessages().isHold()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "ItemInformation":
+      case "ItemInformationResponse":
+        return status.getSupportedMessages().isItemInformation()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "ItemStatusUpdate":
+      case "ItemStatusUpdateResponse":
+        return status.getSupportedMessages().isItemStatusUpdate()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "Login":
+      case "LoginResponse":
+        return status.getSupportedMessages().isLogin()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "PatronEnable":
+      case "PatronEnableResponse":
+        return status.getSupportedMessages().isPatronEnable()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "PatronInformation":
+      case "PatronInformationResponse":
+        return status.getSupportedMessages().isPatronInformation()?status.isStatusUpdateOk()?condition.MESSAGE_VALIDATED:condition.PATRON_STATUS_UPDATE_NOT_SUPPORTED:condition.MESSAGE_NOT_SUPPORTED;
+      case "PatronStatusRequest":
+        return status.getSupportedMessages().isPatronStatusRequest()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "PatronStatusResponse":
+        return status.getSupportedMessages().isBlockPatron() || status.getSupportedMessages().isPatronStatusRequest()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "Renew":
+      case "RenewResponse":
+        return status.getSupportedMessages().isRenew() && status.isCheckOutOk()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+      case "RenewAll":
+      case "RenewAllResponse":
+        return status.getSupportedMessages().isRenewAll() && status.isCheckOutOk()?condition.MESSAGE_VALIDATED:condition.MESSAGE_NOT_SUPPORTED;
+    }
+    return condition.UNKNOWN_MESSAGE;
+  }
+  
+  
+ 
+  
+}
