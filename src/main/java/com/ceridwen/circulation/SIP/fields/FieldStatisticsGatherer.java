20de5f7872edac1fc22eeed4dd2b8f4b6b01fe6e
Matthew J. Dovey <m.dovey@ceridwen.com>
2019-06-18 16:48:04 +0100

    Update licenses

--- src/main/java/com/ceridwen/circulation/SIP/fields/FieldStatisticsGatherer.java
+++ src/main/java/com/ceridwen/circulation/SIP/fields/FieldStatisticsGatherer.java
@@ -1,21 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2010 Matthew J. Dovey (www.ceridwen.com).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at 
- * <http://www.gnu.org/licenses/>
- * 
+/* 
+ * Copyright (C) 2019 Ceridwen Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Contributors:
- *     Matthew J. Dovey (www.ceridwen.com) - initial API and implementation
- ******************************************************************************/
+ */
 package com.ceridwen.circulation.SIP.fields;
 
 import java.util.Enumeration;
--- src/main/java/com/ceridwen/circulation/SIP/messages/CheckOut.java
+++ src/main/java/com/ceridwen/circulation/SIP/messages/CheckOut.java
@@ -1,30 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2010 Matthew J. Dovey (www.ceridwen.com).
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at 
- * <http://www.gnu.org/licenses/>
- * 
+/* 
+ * Copyright (C) 2019 Ceridwen Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Contributors:
- *     Matthew J. Dovey (www.ceridwen.com) - initial API and implementation
- ******************************************************************************/
-/**
- * <p>Title: Self Issue</p>
- * <p>Description: Self Issue Client</p>
- * <p>Copyright: 2004,</p>
- * <p>Company: ceridwen.com</p>
- * @author Matthew J. Dovey
- * @version 2.1
  */
-
 package com.ceridwen.circulation.SIP.messages;
 
 import java.util.Date;
